eeb816e2f866ae14c1591d0caa3b69b9d6412fd0
Leandro Scardua <leandroscar@gmail.com>
2018-01-13 10:38:17 -0500

    Add files via upload

---
+++ tail.ps1
@@ -0,0 +1,17 @@
+﻿Function Get-FileName
+{   
+    [void][System.Reflection.Assembly]::LoadWithPartialName("System.windows.forms")
+    $OpenFileDialog = New-Object System.Windows.Forms.OpenFileDialog
+    $OpenFileDialog.filter = "File (*.*)| *.*" 
+    $OpenFileDialog.initialDirectory = "C:\"
+    $OpenFileDialog.ShowDialog() | Out-Null
+    $OpenFileDialog.filename
+}
+
+$filename = Get-FileName
+
+while(1){
+Get-Content $filename -Tail 20
+    Start-Sleep -Seconds 5
+    Clear
+    }
